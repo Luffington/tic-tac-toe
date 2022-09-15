0be0ad0f2cc4eb9c2f3b45b00fdf1b08773e98d3
Luffington <55111464+Luffington@users.noreply.github.com>
2022-09-15 00:40:32 -0400

    hw posted

---
+++ hw.js
@@ -0,0 +1,23 @@
+// using a while loop, solve the following questions:
+
+// question 1:
+
+// implement the findNumInArray function
+// that takes in a number and an array 
+// as a parameter and returns the index where
+// the number is first seen
+// return -1 if the number is not present
+// in the array
+
+function findNumInArray(num, arr) {}
+
+// question 2
+
+// implement the findNumInMatrix function
+// that takes in a number and an array of 
+// arrays of numbers and returns the
+// coordinates where the number is first seen
+// return -1 if the number is not present in
+// the matrix
+
+function findNumInMatrix(num, matrix) {}
